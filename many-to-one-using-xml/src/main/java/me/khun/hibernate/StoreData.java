c23d01e566bc5f74ce936393003a3a5dc59cba6d
KhunPyaePhyoAung <khunpyaephyoaung74@gmail.com>
2021-01-08 22:40:44 +0630

    add many-to-one-using-xml

---
+++ many-to-one-using-xml/src/main/java/me/khun/hibernate/StoreData.java
@@ -0,0 +1,42 @@
+package me.khun.hibernate;
+
+import me.khun.hibernate.entity.Company;
+import me.khun.hibernate.entity.Employee;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+public class StoreData {
+    public static void main(String[] args) {
+        StandardServiceRegistry standardServiceRegistry = new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();
+        Metadata metadata = new MetadataSources(standardServiceRegistry).buildMetadata();
+        SessionFactory sessionFactory = metadata.buildSessionFactory();
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+
+        Company company1 = new Company();
+        company1.setName("Alphasoft");
+        company1.setType("IT");
+
+        Employee employee1 = new Employee();
+        employee1.setName("Khun Pyae Phyo Aung");
+        employee1.setEmail("khunpyaephyo@gmail.com");
+        employee1.setCompany(company1);
+
+        Employee employee2 = new Employee();
+        employee2.setName("Khun Lar Lay");
+        employee2.setEmail("larlay@gmail.com");
+        employee2.setCompany(company1);
+
+        session.persist(employee1);
+        session.persist(employee2);
+
+        transaction.commit();
+        session.close();
+        System.out.println("Success");
+    }
+}
---
+++ many-to-one-using-xml/src/main/java/me/khun/hibernate/entity/Company.java
@@ -0,0 +1,12 @@
+package me.khun.hibernate.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Company {
+    private Long id;
+    private String name;
+    private String type;
+}
